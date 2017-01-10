fd9c856fca19461bce581b6b8e06b3568e5c971d
johnrahme <john.rahme.se@gmail.com>
2017-01-10 16:43:13 +0100

    Uppdaterat logga och carousel

--- resources/views/layouts/carousel.blade.php
+++ resources/views/layouts/carousel.blade.php
@@ -3,17 +3,26 @@
     <ol class="carousel-indicators">
         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
         <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
 
     </ol>
 
     <!-- Wrapper for slides -->
     <div class="carousel-inner" role="listbox">
         <div class="item active">
-            <img src="img/carousel/Verktyg.jpg" style="width:100%; height: auto" alt="Verktyg">
+            <img src="img/carousel/ÄLGDRÄKTER.jpg" style="width:100%; height: auto" alt="Verktyg">
         </div>
 
         <div class="item">
-            <img src="img/carousel/Skulpturer.jpg" style="width:100%; height: auto" alt="Skulpturer">
+            <img src="img/carousel/Helsingen hem.jpg" style="width:100%; height: auto" alt="Skulpturer">
+        </div>
+        <div class="item">
+            <img src="img/carousel/Indianbild.jpg" style="width:100%; height: auto" alt="Verktyg">
+        </div>
+
+        <div class="item">
+            <img src="img/carousel/Nöthudsväska.jpg" style="width:100%; height: auto" alt="Skulpturer">
         </div>
 
     </div>
